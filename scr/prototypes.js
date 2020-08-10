0ed9e7ec57c3662bfead2e4875eb069104844702
Fluiser <45771430+Fluiser@users.noreply.github.com>
2020-08-10 22:40:05 +0300

    Update prototypes.js

--- scr/prototypes.js
+++ scr/prototypes.js
@@ -1,12 +1,7 @@
 Array.prototype.limitedList = function(limit) {
-    let index = [];
-    for(let i = 0, indexLimit = 0;indexLimit < this.length; ++indexLimit)
-    {
-        if(!index[i]) index[i] = [];
-        if(index[i].length+1 > limit) ++i;
-        if(!index[i]) index[i] = [];
-
-        index[i].push(this[indexLimit]);
+    let index = []
+    for (let i = 0; i < this.length; i+=limit) {
+        index.push(this.slice(i, i + limit));
     }
-    return index;
+    return arr;
 }
