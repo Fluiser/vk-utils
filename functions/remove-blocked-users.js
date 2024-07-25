2bc065ea93f8d0c494e0b27fff0503457455c29f
Alexandr <45771430+Fluiser@users.noreply.github.com>
2024-07-25 16:03:27 +0300

    Update remove-blocked-users.js

--- functions/remove-blocked-users.js
+++ functions/remove-blocked-users.js
@@ -4,13 +4,11 @@ module.exports.name = "remove-users";
 module.exports.description = "Remove blocked user of friends; Has flags: sort(flag): [full, f], [photo], [string, fname, lname]; Method(flag): [block, black, blocklist, blacklist]";
 module.exports.permission = ["friends"];
 
-const fullCheck 			= user => 	user.deactivated ||
-										(user.first_name.match(/[^0-zА-я-\s]/) || user.last_name.match(/[^0-zА-я-\s]/)) ||
-										user.photo_50 === 'https://vk.com/images/camera_50.png';
-const photoCheck 			= user => 	user.deactivated || user.photo_50 === 'https://vk.com/images/camera_50.png';
+const photoCheck 		= user => 	user.deactivated || ['pp.userapi.com/60tZWMo4SmwcploUVl9XEt8ufnTTvDUmQ6Bj1g/mmv1pcj63C4.png', 'vk.com/images/camera_50.png'].some(url => user.photo_50.includes(url));
 const trashStringCheck 		= user => 	user.deactivated ||
-										(user.first_name.match(/[^0-zА-я-\s]/) || user.last_name.match(/[^0-zА-я-\s]/));
-const defaultCheck 			= user => 	user.deactivated;
+						(user.first_name.match(/[^0-zА-я-\s]/) || user.last_name.match(/[^0-zА-я-\s]/));
+const defaultCheck 		= user => 	user.deactivated;
+const fullCheck			= user => 	photoCheck(user) || trashStringCheck(user);
 
 const functionCheck = 	['full', 'f', '--full', '-full'].some(str => process.argv.includes(str)) ? fullCheck :
 						['photo', '--photo', '-photo'].some(str => process.argv.includes(str)) ? photoCheck :
