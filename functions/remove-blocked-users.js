68385d8d6c8a638e65ad6b1520bd230b6c1314c4
Alexandr <45771430+Fluiser@users.noreply.github.com>
2026-01-12 16:21:17 +0300

    Update remove-blocked-users.js

--- functions/remove-blocked-users.js
+++ functions/remove-blocked-users.js
@@ -4,11 +4,11 @@ module.exports.name = "remove-users";
 module.exports.description = "Remove blocked user of friends; Has flags: sort(flag): [full, f], [photo], [string, fname, lname]; Method(flag): [block, black, blocklist, blacklist]";
 module.exports.permission = ["friends"];
 
-const photoCheck 		= user => 	user.deactivated || ['pp.userapi.com/60tZWMo4SmwcploUVl9XEt8ufnTTvDUmQ6Bj1g/mmv1pcj63C4.png', 'vk.com/images/camera_50.png'].some(url => user.photo_50.includes(url));
+const photoCheck 			= user => 	user.deactivated || ['pp.userapi.com/60tZWMo4SmwcploUVl9XEt8ufnTTvDUmQ6Bj1g/mmv1pcj63C4.png', 'vk.com/images/camera_50.png'].some(url => user.photo_50.includes(url));
 const trashStringCheck 		= user => 	user.deactivated ||
-						(user.first_name.match(/[^0-zА-я-\s]/) || user.last_name.match(/[^0-zА-я-\s]/));
-const defaultCheck 		= user => 	user.deactivated;
-const fullCheck			= user => 	photoCheck(user) || trashStringCheck(user);
+										(user.first_name.match(/[^0-zА-я-\s]/) || user.last_name.match(/[^0-zА-я-\s]/));
+const defaultCheck 			= user => 	user.deactivated;
+const fullCheck             = user =>   photoCheck(user) || trashStringCheck(user);
 
 const functionCheck = 	['full', 'f', '--full', '-full'].some(str => process.argv.includes(str)) ? fullCheck :
 						['photo', '--photo', '-photo'].some(str => process.argv.includes(str)) ? photoCheck :
@@ -20,6 +20,22 @@ const functionDelete =  ['block', 'black', 'blocklist', 'blacklist', '-block', '
 
 // console.log(''+functionCheck, '\n', ''+functionDelete); //for test.
 
+async function usersGet(call) {
+    let info = await call("friends.get", {count: 10000});
+    const totalFriends = info.count;
+    let items = info.items;
+
+    for(let i = 1; i < Math.ceil(totalFriends/2500); ++i)
+    {
+        console.log(`${i*2500}/${totalFriends}`);
+        await sleep(2000);
+        const nextitems = await call("friends.get", {count: 2500, offset: i*2500});
+        items = items.concat(nextitems.items);
+    }
+
+    return items;
+}
+
 module.exports.run = async (cfg, call) => {
     // let user = await call("users.get");
     // if(user.error) {
@@ -28,15 +44,19 @@ module.exports.run = async (cfg, call) => {
     // }
     // [user] = user;
 
-    let {items} = await call("friends.get", {count: 10000});
+    let items = await usersGet(call);
     items = items.limitedList(300);
+
     let busers = [];
+    // console.log(items.map(i => i.length),items.map(i => i.length).reduce((a,b) => a+b));
+    // return;
 
     for(let i = 0; i < items.length; ++i)
     {
         console.log(`[received: ${i+1}]/[total: ${items.length}]`)
         let r = await call("users.get", {user_ids: items[i].join(","), fields: 'photo_50'});
         busers = busers.concat(r.filter(functionCheck));
+
         await sleep(2500);
     }
 
